9769752a102a725d95ee1bcf39c5246b3f5a379c
averron82 <averron82@gmail.com>
2017-12-02 11:57:31 +0000

    Added Customer and Waiter prefabs

---
+++ LudumDare40/Assets/Scripts/Customer.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Customer : MonoBehaviour
+{
+    public float MoveSpeed = 1.0f;
+
+    public Transform Leader;
+    public float StartFollowDistance = 1.0f;
+    public float StopFollowDistance = 0.5f;
+
+    bool FollowLeader = false;
+
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+        if (Leader)
+        {
+            Vector3 Position = transform.position;
+            Vector3 LeaderPosition = Leader.position;
+            Vector3 ToLeader = LeaderPosition - Position;
+            float DistanceSq = ToLeader.sqrMagnitude;
+
+            if (DistanceSq >= StartFollowDistance)
+            {
+                FollowLeader = true;
+            }
+            else if (DistanceSq <= StopFollowDistance)
+            {
+                FollowLeader = false;
+            }
+
+            if (FollowLeader)
+            {
+                Vector3 Direction = ToLeader.normalized;
+                Position += Direction * MoveSpeed * Time.deltaTime;
+                transform.position = Position;
+            }
+        }
+    }
+}
---
+++ LudumDare40/Assets/Scripts/Waiter.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Waiter : MonoBehaviour
+{
+    public float MoveSpeed = 1.0f;
+
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+        float Vertical = Input.GetAxis("Vertical");
+        float Horizontal = Input.GetAxis("Horizontal");
+
+        Vector3 position = gameObject.transform.position;
+        position.x += Horizontal * MoveSpeed * Time.deltaTime;
+        position.y += Vertical * MoveSpeed * Time.deltaTime;
+        gameObject.transform.position = position;
+    }
+}
